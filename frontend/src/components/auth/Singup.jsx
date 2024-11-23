dd6180fc047fb26e7d9c6126d5ae5bf556928943
Arun Singh <aruns.ug21.cs@nitp.ac.in>
2024-11-24 02:24:35 +0530

    changes in frontend

--- frontend/src/components/auth/Singup.jsx
+++ frontend/src/components/auth/Singup.jsx
@@ -1,15 +1,15 @@
-import React, { useEffect, useState } from 'react'
-import Navbar from '../shared/Navbar'
-import { Label } from '../ui/label'
-import { Input } from '../ui/input'
-import { Button } from '../ui/button'
-import { RadioGroup, } from '../ui/radio-group'
-import axios from 'axios'
-import { Link, useNavigate } from 'react-router-dom'
-import { toast } from 'sonner'
-import { useDispatch, useSelector } from 'react-redux'
-import { setLoading } from '@/redux/authSlice'
-import { Loader2 } from 'lucide-react'
+import React, { useEffect, useState } from "react";
+import Navbar from "../shared/Navbar";
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import { RadioGroup } from "../ui/radio-group";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
+import { toast } from "sonner";
+import { useDispatch, useSelector } from "react-redux";
+import { setLoading } from "@/redux/authSlice";
+import { Loader2 } from "lucide-react";
 
 const Singup = () => {
   const [input, setInput] = useState({
@@ -20,7 +20,7 @@ const Singup = () => {
     role: "",
     file: "",
   });
-  const { loading, authUser } = useSelector(store => store.auth);
+  const { loading, authUser } = useSelector((store) => store.auth);
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
@@ -29,7 +29,7 @@ const Singup = () => {
   };
   const changeFileHandler = (e) => {
     setInput({ ...input, file: e.target.files?.[0] });
-  }
+  };
 
   const submitHandler = async (e) => {
     e.preventDefault();
@@ -44,12 +44,16 @@ const Singup = () => {
     }
     try {
       dispatch(setLoading(true));
-      const res = await axios.post("http://localhost:8000/api/v1/user/register", formData, {
-        headers: {
-          "Content-Type": "multipart/form-data"
-        },
-        withCredentials: true
-      });
+      const res = await axios.post(
+        "https://jobportal-x4fb.onrender.com/api/v1/user/register",
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+          withCredentials: true,
+        }
+      );
       if (res.data.success) {
         navigate("/login");
         toast.success(res.data.message);
@@ -60,21 +64,24 @@ const Singup = () => {
     } finally {
       dispatch(setLoading(false));
     }
-  }
+  };
   useEffect(() => {
-    if (authUser?.role === 'recruiter') {
+    if (authUser?.role === "recruiter") {
       navigate("/admin/companies");
-    } else if (authUser?.role === 'student') {
+    } else if (authUser?.role === "student") {
       navigate("/");
     }
-  }, [])
+  }, []);
   return (
     <>
       <Navbar />
-      <div className='flex items-center justify-center max-w-7xl mx-auto'>
-        <form onSubmit={submitHandler} className='w-1/2 border border-gray-200 rounded-md p-4 my-10'>
-          <h1 className='font-bold text-xl mb-4'>Sign Up</h1>
-          <div className='my-2'>
+      <div className="flex items-center justify-center max-w-7xl mx-auto">
+        <form
+          onSubmit={submitHandler}
+          className="w-1/2 border border-gray-200 rounded-md p-4 my-10"
+        >
+          <h1 className="font-bold text-xl mb-4">Sign Up</h1>
+          <div className="my-2">
             <Label>Full Name</Label>
             <Input
               type="text"
@@ -84,7 +91,7 @@ const Singup = () => {
               placeholder="patel"
             />
           </div>
-          <div className='my-2'>
+          <div className="my-2">
             <Label>Email</Label>
             <Input
               type="email"
@@ -94,7 +101,7 @@ const Singup = () => {
               placeholder="patel@gmail.com"
             />
           </div>
-          <div className='my-2'>
+          <div className="my-2">
             <Label>Phone Number</Label>
             <Input
               type="text"
@@ -104,7 +111,7 @@ const Singup = () => {
               placeholder="+918080808080"
             />
           </div>
-          <div className='my-2'>
+          <div className="my-2">
             <Label>Password</Label>
             <Input
               type="password"
@@ -114,14 +121,14 @@ const Singup = () => {
               placeholder="password"
             />
           </div>
-          <div className='flex items-center justify-between'>
+          <div className="flex items-center justify-between">
             <RadioGroup className="flex items-center gap-4 my-5">
               <div className="flex items-center space-x-2">
                 <Input
                   type="radio"
                   name="role"
                   value="student"
-                  checked={input.role === 'student'}
+                  checked={input.role === "student"}
                   onChange={changeEventHandler}
                   className="cursor-pointer"
                 />
@@ -132,14 +139,14 @@ const Singup = () => {
                   type="radio"
                   name="role"
                   value="recruiter"
-                  checked={input.role === 'recruiter'}
+                  checked={input.role === "recruiter"}
                   onChange={changeEventHandler}
                   className="cursor-pointer"
                 />
                 <Label htmlFor="r2">Recruiter</Label>
               </div>
             </RadioGroup>
-            <div className='flex items-center gap-2'>
+            <div className="flex items-center gap-2">
               <Label>Profile</Label>
               <Input
                 accept="image/*"
@@ -149,21 +156,29 @@ const Singup = () => {
               />
             </div>
           </div>
-          {
-            loading ? (
-              <Button className='w-full my-4'>
-                <Loader2 className='mr-2 h-4 w-4 animate-spin' />
-                Please wait
-              </Button>
-            ) : (
-              <Button type='submit' className='w-full my-4'>Sign Up</Button>
-            )
-          }
-          <span className='text-sm'>Already have an account? <Link to={"/login"} className='text-blue-500 cursor-pointer underline'>Login</Link></span>
+          {loading ? (
+            <Button className="w-full my-4">
+              <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+              Please wait
+            </Button>
+          ) : (
+            <Button type="submit" className="w-full my-4">
+              Sign Up
+            </Button>
+          )}
+          <span className="text-sm">
+            Already have an account?{" "}
+            <Link
+              to={"/login"}
+              className="text-blue-500 cursor-pointer underline"
+            >
+              Login
+            </Link>
+          </span>
         </form>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Singup
+export default Singup;
